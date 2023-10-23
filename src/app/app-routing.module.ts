6c6893a9df22611a2f480c216fcf03727e84085a
JaaferJaafer4 <jjaafar006@gmail.com>
2023-10-23 22:25:44 +0100

    skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { InscriptionsComponent } from './components/inscriptions/inscriptions.component';
+import { CalendarsComponent } from './components/calendars/calendars.component';
+import { EvaluationsComponent } from './components/evaluations/evaluations.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'users',component:UsersComponent},
+  {path:'inscriptions',component:InscriptionsComponent},
+  {path:'calendars',component:CalendarsComponent},
+  {path:'evaluations',component:EvaluationsComponent},
+  {path:'courses',component:CoursesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UsersComponent } from './components/users/users.component';
+import { CalendarsComponent } from './components/calendars/calendars.component';
+import { EvaluationsComponent } from './components/evaluations/evaluations.component';
+import { InscriptionsComponent } from './components/inscriptions/inscriptions.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UsersComponent,
+    CalendarsComponent,
+    EvaluationsComponent,
+    InscriptionsComponent,
+    CoursesComponent
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
